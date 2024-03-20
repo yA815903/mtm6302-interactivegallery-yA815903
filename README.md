5269c34f76e7faa55e4544f8680b6bc3514199cd
Gerry Adams-Gaumont <adam0354@algonquinlive.com>
2024-03-19 20:47:55 -0400

    Completes the assignment

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mtm6302-interactivegallery-yA815903
 Gerry Adams-Gaumont
 number  of students: 040820263
+ citation D. M. (n.d.). Lorem Picsum. Lorem Picsum. Retrieved March 19, 2024, from https://picsum.photos
--- script.js
+++ script.js
@@ -1,60 +1,53 @@
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
 const images = [
-    {
-        id: "battle",
-        src: "images/battle.jpg",
-        hdSrc: "images/battle_hd.jpg",
-        caption: "Image 1"
-    },
-    {
-        id: "last-of-us",
-        src: "images/last-of-us.jpg",
-        hdSrc: "images/last-of-us_hd.jpg",
-        caption: "Image 2"
-    },
-    {
-        id:"viking-human",
-        src: "images/viking-human.jpg",
-        hdSrc: "images/viking-human_hd.jpg",
-        caption: "Image 3"
-    },
-    {
-        id: "viking",
-        src: "images/viking.jpg",
-        hdSrc: "images/viking_hd.jpg",
-        caption: "Image 4"
-    },
+    { id: 'image1', src: 'https://picsum.photos/id/1015/400/300', hdSrc: 'https://picsum.photos/id/1015/1200/900', caption: 'Image 1' },
+    { id: 'image2', src: 'https://picsum.photos/id/1018/400/300', hdSrc: 'https://picsum.photos/id/1018/1200/900', caption: 'Image 2' },
+    { id: 'image3', src: 'https://picsum.photos/id/1020/400/300', hdSrc: 'https://picsum.photos/id/1020/1200/900', caption: 'Image 3' },
+    { id: 'image4', src: 'https://picsum.photos/id/1022/400/300', hdSrc: 'https://picsum.photos/id/1022/1200/900', caption: 'Image 4' },
+    { id: 'image1', src: 'https://picsum.photos/id/1023/400/300', hdSrc: 'https://picsum.photos/id/1015/1200/900', caption: 'Image 1' },
+    { id: 'image2', src: 'https://picsum.photos/id/1024/400/300', hdSrc: 'https://picsum.photos/id/1024/1200/900', caption: 'Image 2' },
+    { id: 'image3', src: 'https://picsum.photos/id/1025/400/300', hdSrc: 'https://picsum.photos/id/1025/1200/900', caption: 'Image 3' },
+    { id: 'image4', src: 'https://picsum.photos/id/1026/400/300', hdSrc: 'https://picsum.photos/id/1026/1200/900', caption: 'Image 4' },
 ];
 
-images.forEach((image) => {
-    const img = document.createElement("img");
+function createImageElement(image) {
+    const img = document.createElement('img');
     img.src = image.src;
     img.dataset.hdSrc = image.hdSrc;
     img.dataset.caption = image.caption;
     img.alt = image.caption;
-    img.addEventListener("click", showHDImage);
-    gallery.appendChild(img);
-});
+    img.addEventListener('click', showHDImage);
+    return img;
+}
 
 function showHDImage(event) {
     const img = event.target;
-    const hdImage = document.createElement("div");
-    hdImage.className = "hd-image";
+    const hdImage = document.createElement('div');
+    hdImage.className = 'hd-image';
     hdImage.innerHTML = `<img src="${img.dataset.hdSrc}" alt="${img.dataset.caption}">`;
-    hdImage.addEventListener("click",HDImage); // Attach the event listener to the hdImage div
-    const caption = document.createElement("div");
-    caption.className = "caption";
+    hdImage.addEventListener('click', hideHDImage);
+    const caption = document.createElement('div');
+    caption.className = 'caption';
     caption.textContent = img.dataset.caption;
-    gallery.appendChild(hdImage); // Append the hdImage div to the gallery
-    gallery.appendChild(caption); // Append the caption div to the gallery
-    hdImage.style.display = "block";
-    caption.style.display ="block";
+    hdImage.appendChild(caption);
+    gallery.appendChild(hdImage);
+    hdImage.style.display = 'block';
+    caption.style.display = 'block';
 }
 
 function hideHDImage(event) {
-    const hdImage = event.target.closest(".hd-image"); // Get the closest ancestor with the class "hd-image"
-    const caption = document.querySelector(".caption");
-    hdImage.style.display = "none";
-    caption.style.display = "none";
+    const hdImage = event.target.closest('.hd-image');
+    hdImage.style.display = 'none';
+    const caption = hdImage.querySelector('.caption');
+    caption.style.display = 'none';
 }
+
+function addImagesToGallery() {
+    images.forEach(image => {
+        const img = createImageElement(image);
+        gallery.appendChild(img);
+    });
+}
+
+addImagesToGallery();
